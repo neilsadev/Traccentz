c2844ab9585c617dac3946eea01da16abbbdc6bc
neiloy <neiloyhassan1@gmail.com>
2023-07-20 02:41:49 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/traccentz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.traccentz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/expense_data.dart
@@ -0,0 +1,11 @@
+class ExpenseData {
+  // list of ALL expenses
+  // get expense list
+  // add new expense
+  // delete expense
+  // get weekday (mon, tues, etc) from a dateTime object
+  // get the date for the start of the week ( sunday )
+  //convert overall list of expenses into a daily expense summary
+  //e.g.
+  //overallExpenseList =[]
+}
